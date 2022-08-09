7bc0f91d99019a1a805c94ad088bb65803602f49
Matas <m.tunkevicius@gmail.com>
2022-08-09 15:38:28 +0300

    save progress

---
+++ ImperialEngine/ImperialEngine/src/frontend/Components/Components.h
@@ -0,0 +1,20 @@
+#include <GLM/mat4x4.hpp>
+#include <ENTT/entt.hpp>
+
+namespace Comp
+{
+	struct Transform
+	{
+		glm::mat4x4 transform;
+	};
+
+	struct Material
+	{
+
+	};
+
+	struct Mesh
+	{
+		entt::entity e;
+	};
+}
